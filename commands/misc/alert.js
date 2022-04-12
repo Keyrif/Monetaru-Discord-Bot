386ac6d86b7d339476b7a0939e19362d1c255afe
Keyrif <83619468+Keyrif@users.noreply.github.com>
2022-04-12 10:02:22 -0700

    Create alert.js

---
+++ commands/misc/alert.js
@@ -0,0 +1,39 @@
+const { MessageEmbed } = require('discord.js');
+const mongoose = require('mongoose');
+const Guild = require('../../guild');
+const color = process.env.COLOR;
+const prefix = process.env.PREFIX;
+const mongo = require('../../mongo')
+const commandPrefixSchema = require('../../schemas/command-prefix-schema')
+
+module.exports = {
+    commands: ['alert'],
+      permissions: 'ADMINISTRATOR',
+    callback: async (message) => {
+        if (!message.guild.id === '899340027719479347') {
+            messag.reply('Aceasta comanda nu poate fi executata.')
+        }
+        if (message.guild.id === '899340027719479347') {
+            if (!message.member.roles.cache.some(role => role.name.includes('ALERT'))) {
+                let role = message.guild.roles.cache.find(r => r.id === "919999343145127957");
+                message.guild.member(message.author).roles.add(role);
+                const embed2 = new MessageEmbed()
+                .setColor("#00ff00")
+                .setTitle('ALERT')
+                .setDescription("<:verde:909863626771410974> | **ENABLED**")
+                .setFooter(`${message.author.tag}`, `${message.author.displayAvatarURL({ dynamic: true, size: 1024 })}`)
+                message.channel.send(embed2);
+              }
+            if (message.member.roles.cache.some(role => role.name.includes('ALERT'))) {
+                let role = message.guild.roles.cache.find(r => r.id === "919999343145127957");
+                message.guild.member(message.author).roles.remove(role);
+                const embed2 = new MessageEmbed()
+                .setColor("ff0000")
+                .setTitle('ALERT')
+                .setDescription("<:rosu:909865200046473256> | **DISABLED**")
+                .setFooter(`${message.author.tag}`, `${message.author.displayAvatarURL({ dynamic: true, size: 1024 })}`)
+                message.channel.send(embed2);
+              }
+        }
+    }
+}
