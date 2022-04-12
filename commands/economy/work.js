7b772fbceb4d2592d1aa8620a665492ace11a58e
Keyrif <83619468+Keyrif@users.noreply.github.com>
2022-04-12 10:06:29 -0700

    Create work.js

---
+++ commands/economy/work.js
@@ -0,0 +1,73 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+const { MessageEmbed } = require('discord.js');
+const mongoose = require('mongoose');
+const Guild = require('../../guild');
+const color = process.env.COLOR;
+const prefix = process.env.PREFIX;
+const quiz = require('../../quiz.json');
+const humanizeDuration = require('humanize-duration');
+const cooldowns = new Map();
+const economy = require('../../economy')
+module.exports = {
+  commands: ['work'],
+  expectedArgs: ".",
+  callback: async (message, arguments) => {
+    const arrayOfUsersIds = ['8794328794879328794897', '789438729489732789'];
+
+for (let i = 0; i < arrayOfUsersIds.length; i++) {
+    if (message.author.id === arrayOfUsersIds[i]) return message.reply('❌ **Nu poti folosi comenzile de economie!**\n**REASON: blacklist**');
+};
+    let user = message.author;
+    const item = quiz[Math.floor(Math.random() * quiz.length)];
+    //const filter = m => (m.content.includes('discord') && m.author.id != client.user.id);
+    const filter = response => {
+      return item.answers.some(answer => answer.toLowerCase() === response.content.toLowerCase() && response.author.id === user.id);
+    };
+    const collector = message.channel.createMessageCollector(filter, { time: 15000 });
+
+    collector.on('collect', m => {
+      console.log(`Collected ${m.content}`);
+    });
+
+    collector.on('end', collected => {
+      console.log(`Collected ${collected.size} items`);
+    });
+    const cooldown = cooldowns.get(message.author.id);
+    if (cooldown) {
+      const remaining = humanizeDuration(cooldown - Date.now());
+
+return message.channel.send(`<:bump:909822887404380240> Asteapta fraticu **${remaining}** ca sa muncesti iar.`)
+        .catch(console.error);
+    }
+    const embed = new MessageEmbed()
+      .setColor(color)
+      .setTitle('Working...')
+      .setDescription(item.question)
+      .setThumbnail('https://i.imgur.com/qTfipOV.png')
+      .setFooter(`${message.author.tag}`, `${message.author.displayAvatarURL({ dynamic: true, size: 1024 })}`)
+    message.channel.send(embed).then(() => {
+      message.channel.awaitMessages(filter, { max: 1, time: 10000, errors: ['time'] })
+        .then(collected => {
+          const randomNumber = Math.floor(Math.random() * 51) + 50;
+          const coins = randomNumber
+      
+          const guildId = message.guild.id
+          const userId = message.author.id
+          const newCoins = economy.addCoins(guildId, userId, coins)
+          const embed2 = new MessageEmbed()
+          .setColor(color)
+          .setTitle('Work')
+          .setThumbnail('https://i.imgur.com/qTfipOV.png')
+          .addField(`Ai muncit ca ultimul sclav si ai primit:`, `+ ${coins} <:newccoin:911788039603359774>`, true)
+          .setFooter(`${message.author.tag}`, `${message.author.displayAvatarURL({ dynamic: true, size: 1024 })}`)
+          message.channel.send(embed2);
+        })
+        .catch(collected => {
+          message.channel.send(`${message.author}, ti-a expirat timpul mosule.`);
+        });
+    });
+    cooldowns.set(message.author.id, Date.now() + 15000);
+    setTimeout(() => cooldowns.delete(message.author.id), 15000);
+  }
+}
