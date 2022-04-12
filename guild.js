a3154ba0a399569ee3b22f5720314d5418675e09
Keyrif <83619468+Keyrif@users.noreply.github.com>
2022-04-12 09:56:50 -0700

    Create guild.js

---
+++ guild.js
@@ -0,0 +1,14 @@
+//
+//  (!) THIS CODE SAW MANY CHANGES SO THERE CAN BE SOME JUNK LEFT HERE AND THERE. SORRY! NOTHING IS FATAL THOUGH, EVERYTHING IS WORKING FLAWLESSLY!
+//
+
+const mongoose = require('mongoose');
+
+const guildSchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    guildID: String,
+    guildName: String,
+    prefix: String
+});
+
+module.exports = mongoose.model('Guild', guildSchema, 'guilds');
