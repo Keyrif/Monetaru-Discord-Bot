4780a5862629f9211cd67fefcd49a1e1dea7960c
Keyrif <83619468+Keyrif@users.noreply.github.com>
2022-04-12 09:53:08 -0700

    Create command.js

---
+++ command.js
@@ -0,0 +1,24 @@
+//
+//  (!) THIS CODE SAW MANY CHANGES SO THERE CAN BE SOME JUNK LEFT HERE AND THERE. SORRY! NOTHING IS FATAL THOUGH, EVERYTHING IS WORKING FLAWLESS!
+//
+
+const prefix = process.env.PREFIX;
+
+module.exports = (client, aliases, callback) => {
+  if (typeof aliases === 'string') {
+    aliases = [aliases]
+  }
+
+  client.on('message', (message) => {
+    const { content } = message
+
+    aliases.forEach((alias) => {
+      const command = `${prefix}${alias}`
+
+      if (content.startsWith(`${command} `) || content === command) {
+        console.log(`Running the command ${command}`)
+        callback(message)
+      }
+    })
+  })
+}
